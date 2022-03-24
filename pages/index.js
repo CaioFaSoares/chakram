cfa5b7ed48ec5c9b7eb98ad59880a208a4dfb5f5
Caio <caiofasoares@gmail.com>
2022-03-23 23:06:20 -0300

    installing gitkraken again

--- pages/index.js
+++ pages/index.js
@@ -3,14 +3,14 @@ import { Container, Box, Heading } from '@chakra-ui/react'
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+            <Box borderRadius="lg" bg="tomato" p={3} mt={3} mb={6} align="center">
                 Hello, I&apos;m a designer and front-end developer based in Brazil!
             </Box>
 
             <Box display={{md:'flex'}}>
             <Box flexGrow={1}>
                 <Heading as="h2" variant="page-title">
-                    Caio F. Soares
+                    Caio Soares
                 </Heading>
                 <p>
                     Designer and Developer based in Fortaleza, Brazil.
